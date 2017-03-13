fa22c0f37347f0f1ca2f5e60197e40ee86c657f0
Dave Forgac <tylerdave@tylerdave.com>
2017-03-13 19:03:38 -0400

    fix formatting

--- Fire/calculator.py
+++ Fire/calculator.py
@@ -8,11 +8,11 @@ class Calculator(object):
     def add(self, number1, number2):
         """Add two numbers."""
         return number1 + number2
-    
+
     def double(self, number):
         """Multiply input x2."""
         return 2 * number
-    
+
 
 if __name__ == '__main__':
     fire.Fire(Calculator)
