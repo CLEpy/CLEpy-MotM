ac8db1ddbbbd1047c106f83bac758050d5c0b8c5
mcorrand <matt.corrand@gmail.com>
2018-04-09 13:06:42 -0400

    v2

--- django_fsm/talk/app/models.py
+++ django_fsm/talk/app/models.py
@@ -9,3 +9,12 @@ class Order(models.Model):
     address = models.CharField(max_length=1000)
     item = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=9, decimal_places=2)
+
+    # Customers should be able to cancel their order, duh!
+    is_cancelled = models.BooleanField(default=False)
+
+    def cancel(self):
+        # TODO: worry about that later!
+        # self.refund()
+        self.is_cancelled = True
+        self.save()
