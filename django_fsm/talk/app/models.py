d7678e6711b992716dbc06bdbb9d1037a3e82162
mcorrand <matt.corrand@gmail.com>
2018-04-09 12:59:01 -0400

    v1

--- django_fsm/talk/app/models.py
+++ django_fsm/talk/app/models.py
@@ -3,4 +3,9 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Order(models.Model):
+    customer = models.CharField(max_length=255)
+    address = models.CharField(max_length=1000)
+    item = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=9, decimal_places=2)
